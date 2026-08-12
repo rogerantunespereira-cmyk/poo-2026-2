b9ed86114261a18115a9ed4a9c39458db75a0093
rogerantunespereira-cmyk <rogerantunespereira@gmail.com>
2026-08-12 16:55:20 -0300

    Aula01-12/08/2026

---
+++ aula_01/FichaPessoal.java
@@ -0,0 +1,13 @@
+public class FichaPessoal {
+
+    public static void main(String[]args) {
+
+        
+
+        int DataNascimento = 2009;
+        int AnoAtual = 2026;
+        int Idade = AnoAtual - DataNascimento;
+
+        System.out.println(Idade);
+    }
+}
